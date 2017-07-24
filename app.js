af29df1247d358d6b54ae2d0e43ddb18aca374fc
Anand <Anand@Anands-MacBook-Pro.local>
2017-07-24 23:47:10 +0530

    Base

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/api/data', function (req, res) {
+  console.log(req.body)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
